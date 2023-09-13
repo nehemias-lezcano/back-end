289d12a8f57c1518a2aabc0fb7b1de2cec6d1fb2
nehemias lezcano <nlezcano2001@gmail.com>
2023-09-13 16:56:00 -0300

    correccion de primer entrega

--- app.js
+++ app.js
@@ -1,44 +1,61 @@
-
 class ProductManager {
     constructor() {
-        this.products = [];
-    }
-
-    addProduct(title, description, price, thumbnail, stock) {
-        const product = {
-            id: !this.products.length ? 1 : this.products[this.products.length - 1].id + 1,
-            title,
-            description,
-            price,
-            thumbnail,
-            stock
-        };
-        this.products.push(product);
-    }
-
-    getProductById(id) {
-        const product = this.products.find(product => product.id === id);
-
-        if (product) {
-            return product;
-        } else {
-            console.error("Product not found");
-            return null; // Puedes devolver null u otro valor indicativo de que el producto no se encontró
-        }
-    }
+    this.products = []
 }
 
-// Ejemplo de uso:
-const productManager = new ProductManager();
-productManager.addProduct("Producto 1", "Descripción del producto 1", 19.99, "imagen1.jpg", 10);
-productManager.addProduct("Producto 2", "Descripción del producto 2", 29.99, "imagen2.jpg", 5);
-
-const productId = 1;
-const foundProduct = productManager.getProductById(productId);
-
-if (foundProduct) {
-    console.log("Product found:", foundProduct);
-} else {
-    console.log("Product not found");
+    getProducts() {
+    console.log(this.products);
+    return this.products
 }
 
+getProductById(idProduct) {
+    const product = this.products.find((p) => p.id === idProduct)
+    if (!product) {
+    console.log('Not found')
+    return
+}
+    return product
+}
+
+addProduct(product) {
+    const { name, price, stock, code } = product
+    if (!name || !price || !stock || !code) {
+    console.log('Some data is missing')
+    return
+}
+    const isCodeRepeat = this.products.some((p) => p.code === code)
+    if (isCodeRepeat) {
+        console.log('Code already used')
+        return
+}
+    let id
+    if (!this.products.length) {
+    id = 1
+
+    } else {
+    id = this.products[this.products.length - 1].id + 1
+    }
+    
+    const newProduct = { id, ...product }
+    this.products.push(newProduct)
+    console.log('Product added')
+    return newProduct
+}
+} /* fin del product manager*/
+
+
+  // product  = {name,price,stock,code}
+const manager = new ProductManager()
+manager.addProduct({
+    name: 'Samsung',
+    price: 5000,
+    code: '4535sdfsdfsd',
+    stock: 16,
+})
+  manager.addProduct({
+    name: 'Tablet samsung',
+    price: 3000,
+    code: '45645cvxvdf',
+    stock: 13,
+    })
+    manager.getProducts()
