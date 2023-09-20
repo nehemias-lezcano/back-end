1adc7eec737c67f60f1755d79cf457cf3f86c076
nehemias lezcano <nlezcano2001@gmail.com>
2023-09-20 16:52:10 -0300

    entregable 2

--- app.js
+++ app.js
@@ -1,61 +1,104 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
-    this.products = []
-}
+    constructor(filePath) {
+        this.path = filePath;
+        this.products = [];
+        this.loadProducts();
+    }
+
+    loadProducts() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            this.products = JSON.parse(data);
+        } catch (error) {
+            this.products = [];
+        }
+    }
+
+    saveProducts() {
+        const data = JSON.stringify(this.products, null, 2);
+        fs.writeFileSync(this.path, data);
+    }
 
     getProducts() {
-    console.log(this.products);
-    return this.products
-}
-
-getProductById(idProduct) {
-    const product = this.products.find((p) => p.id === idProduct)
-    if (!product) {
-    console.log('Not found')
-    return
-}
-    return product
-}
-
-addProduct(product) {
-    const { name, price, stock, code } = product
-    if (!name || !price || !stock || !code) {
-    console.log('Some data is missing')
-    return
-}
-    const isCodeRepeat = this.products.some((p) => p.code === code)
-    if (isCodeRepeat) {
-        console.log('Code already used')
-        return
-}
-    let id
-    if (!this.products.length) {
-    id = 1
-
-    } else {
-    id = this.products[this.products.length - 1].id + 1
+        return this.products;
     }
-    
-    const newProduct = { id, ...product }
-    this.products.push(newProduct)
-    console.log('Product added')
-    return newProduct
+
+    getProductById(idProduct) {
+        const product = this.products.find((p) => p.id === idProduct);
+        return product;
+    }
+
+    addProduct(product) {
+        const { title, description, price, thumbnail, code, stock } = product;
+
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log('Some data is missing');
+            return;
+        }
+
+        const isCodeRepeat = this.products.some((p) => p.code === code);
+        if (isCodeRepeat) {
+            console.log('Code already used');
+            return;
+        }
+
+        const id = this.products.length > 0 ? Math.max(...this.products.map((p) => p.id)) + 1 : 1;
+
+        const newProduct = { id, title, description, price, thumbnail, code, stock };
+        this.products.push(newProduct);
+        this.saveProducts(); // Guardar en el archivo
+        console.log('Product added');
+        return newProduct;
+    }
+
+    updateProduct(idProduct, updatedFields) {
+        const productIndex = this.products.findIndex((p) => p.id === idProduct);
+
+        if (productIndex === -1) {
+            console.log('Product not found');
+            return;
+        }
+
+        this.products[productIndex] = { ...this.products[productIndex], ...updatedFields };
+        this.saveProducts(); // Guardar en el archivo
+        console.log('Product updated');
+        return this.products[productIndex];
+    }
+
+    deleteProduct(idProduct) {
+        const productIndex = this.products.findIndex((p) => p.id === idProduct);
+
+        if (productIndex === -1) {
+            console.log('Product not found');
+            return;
+        }
+
+        this.products.splice(productIndex, 1);
+        this.saveProducts(); // Guardar en el archivo
+        console.log('Product deleted');
+    }
 }
-} /* fin del product manager*/
 
+const manager = new ProductManager('products.json');
 
-  // product  = {name,price,stock,code}
-const manager = new ProductManager()
 manager.addProduct({
-    name: 'Samsung',
+    title: 'Samsung',
+    description: 'Smartphone',
     price: 5000,
+    thumbnail: 'samsung.jpg',
     code: '4535sdfsdfsd',
     stock: 16,
-})
-  manager.addProduct({
-    name: 'Tablet samsung',
+});
+
+manager.addProduct({
+    title: 'Tablet Samsung',
+    description: 'Tablet',
     price: 3000,
+    thumbnail: 'tablet.jpg',
     code: '45645cvxvdf',
     stock: 13,
-    })
-    manager.getProducts()
+});
+
+console.log(manager.getProducts());
